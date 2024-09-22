bd92c896ab13427739496f0fe12b057878429963
tatianekovalek <126890463+tatianekovalek@users.noreply.github.com>
2024-09-22 09:29:51 -0300

    sla

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "De acordo com suas escolhas...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
